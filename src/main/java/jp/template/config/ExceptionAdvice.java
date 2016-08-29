a14ad025e7bb197b3777992e73adb4fd5a1ecf88
kiyotake hosomi <kiyotake.hosomi@outlook.jp>
2016-08-29 17:56:10 +0900

    システムエラー時、ハンドリング。 - 共通のエラー処理実装（ログ出力）。 - エラー表示簡易画面実装。

---
+++ src/main/java/jp/template/config/ExceptionAdvice.java
@@ -0,0 +1,33 @@
+package jp.template.config;
+
+import java.security.Principal;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+
+	/** Logger. */
+	private static Logger logger = LogManager.getLogger();
+	
+	@ExceptionHandler(value = { Exception.class, RuntimeException.class })
+	public ModelAndView defaultErrorHandler(Principal principal, HttpServletRequest request, Exception e) {
+		ModelAndView mav = new ModelAndView("error");
+
+		logger.error("error-url:{},loginid:{}", request.getRequestURL(), principal.getName(), e);
+
+		mav.addObject("datetime", new Date());
+		mav.addObject("exception", e);
+
+		mav.addObject("url", request.getRequestURL());
+		return mav;
+	}
+	
+}
